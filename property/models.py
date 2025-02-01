3d879944e6b12b3dab2ab3f427eec0712ee59a7e
MaksimObozniy <mobozn@gmail.com>
2025-02-01 07:58:38 +0400

    fixed data duplication

--- property/models.py
+++ property/models.py
@@ -5,8 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-
     new_building = models.BooleanField(
         'Новое здание',
         null=True,
