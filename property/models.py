ac67d3d1e40caffc7a4bc825c0640b77ad0de816
MaksimObozniy <mobozn@gmail.com>
2025-02-16 16:42:18 +0400

    Eliminated duplication of data in the database

--- property/models.py
+++ property/models.py
@@ -5,14 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owner_pure_phone = PhoneNumberField(
-        'Нормализированный номер телефона',
-        blank=True,
-        null=True,
-        region='RU'
-    )
     new_building = models.BooleanField(null=True, blank=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
